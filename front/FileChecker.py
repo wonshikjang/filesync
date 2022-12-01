171d25576d321be93da3370a41801b562fcbb064
99yuseong <dbtjd6511@gmail.com>
2022-12-02 06:56:27 +0900

    change form of file class and create Root/path

--- front/FileChecker.py
+++ front/FileChecker.py
@@ -1,5 +1,6 @@
 from File import File, FileList
-import os
+import asyncio
+import os, re
 try:
     from watchdog.observers import Observer
     from watchdog.events import RegexMatchingEventHandler
@@ -8,12 +9,28 @@ except ModuleNotFoundError as e:
     os.system("pip install watchdog")
 
 class FileChecker(RegexMatchingEventHandler):
-    def __init__(self, _target, _logger):
-        super().__init__(None, [".*\.py"], False, False)
+    def __init__(self, _target, _logger, _api):
+        super().__init__(None, [".*\.py",".*\.pyc",".*\.ini"], False, False)
         self.target = _target
-        self.filelist = FileList()
+        self.filelist = FileList(self.target)
         self.observer = self.setObserver(_target)
         self.logger = _logger
+        self.api = _api
+        # self.getfiles()
+
+    # def getNeedToUpdate(self):
+    #     need_update = asyncio.run(self.api.getFileList())
+        
+    #     # 기존 로컬 파일 탐색
+    #     for (path, dir, file) in os.walk(self.target):
+    #         for f in file:
+    #             f_loc = self.filelist.append("%s/%s" % (path, f))
+                
+    #             for f_ser in need_update:
+    #                 if f_loc.path == f_ser["path"] and f_loc.md5 == f_ser["md5"]:
+    #                     need_update.remove(f_ser)
+    #                     break
+    #     return need_update
     
     def setObserver(self, target):
         observer = Observer()
@@ -32,11 +49,10 @@ class FileChecker(RegexMatchingEventHandler):
             self.logger.print_log("MOVE ERROR")
             return
         else:
-            self.logger.print_log("File '%s' moved \n\t\t\tfrom %s \n\t\t\tto   %s" % (f.name, event.src_path, event.dest_path))
+            self.logger.print_log("File '%s' moved \n\t\t\tfrom %s \n\t\t\tto   %s" % (f.name, re.sub(self.target, "Root", event.src_path), re.sub(self.target, "Root", event.dest_path)))
     
     def on_deleted(self, event):
         if event.is_directory:
-            print(event)
             fs = self.filelist.del_dir(event.src_path)
             if fs == -1:
                 self.logger.print_log("DELETE DIR ERROR")
