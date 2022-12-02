25cd7b868416fb6daf400f96374e78d8efaa69ba
장원식 <wonsik@Ethan-MacBookPro.local>
2022-12-02 20:06:26 +0900

    gitignore, upload file type remove

--- back/main.py
+++ back/main.py
@@ -105,7 +105,8 @@ async def upload_photo(file: UploadFile):
     UPLOAD_DIR = "./static"  # 이미지를 저장할 서버 경로
 
     content = await file.read()
-    filename = f"{str(uuid.uuid4())}.jpg"  # uuid로 유니크한 파일명으로 변경
+    filename = file.filename
+    #filename = f"{str(uuid.uuid4())}.jpg"  # uuid로 유니크한 파일명으로 변경
     with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
         fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
 
